831685f88f79a090ea61a537ba2d2556b4d1e0c4
Alvin Lee <alvinklee@gmail.com>
2013-01-09 09:45:20 -0800

    first working copy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blacklight2::Application.config.secret_token = '5ff9ae720e02d837e1230b29b02ab450d7de247792a74cded102de8caa901bf7b6d0a6d57401dcd473834ac0f85457dc58c1782ce2a55ec6f76d32457cd4fc0d'
